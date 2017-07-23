edab82289e3473722ab3eae54a93bc58bbb1def0
David Whitlock <david.m.whitlock@gmail.com>
2017-07-23 14:58:51 -0700

    Removed last vestiges of the airline application.

--- minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
+++ minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
@@ -1,30 +1,5 @@
 package edu.pdx.cs410J.katas;
 
-import edu.pdx.cs410J.AbstractFlight;
+public class Minesweeper  {
 
-public class Minesweeper extends AbstractFlight {
-  @Override
-  public int getNumber() {
-    return 42;
-  }
-
-  @Override
-  public String getSource() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
-
-  @Override
-  public String getDepartureString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
-
-  @Override
-  public String getDestination() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
-
-  @Override
-  public String getArrivalString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
 }
--- minesweeper/src/test/java/edu/pdx/cs410J/katas/MinesweeperTest.java
+++ minesweeper/src/test/java/edu/pdx/cs410J/katas/MinesweeperTest.java
@@ -1,31 +1,9 @@
 package edu.pdx.cs410J.katas;
 
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 /**
  * Unit tests for the {@link Minesweeper} class.
  */
 public class MinesweeperTest {
   
-  @Test(expected = UnsupportedOperationException.class)
-  public void getArrivalStringNeedsToBeImplemented() {
-    Minesweeper flight = new Minesweeper();
-    flight.getArrivalString();
-  }
 
-  @Test
-  public void initiallyAllFlightsHaveTheSameNumber() {
-    Minesweeper flight = new Minesweeper();
-    assertThat(flight.getNumber(), equalTo(42));
-  }
-
-  @Test
-  public void forProject1ItIsOkayIfGetDepartureTimeReturnsNull() {
-    Minesweeper flight = new Minesweeper();
-    assertThat(flight.getDeparture(), is(nullValue()));
-  }
-  
 }

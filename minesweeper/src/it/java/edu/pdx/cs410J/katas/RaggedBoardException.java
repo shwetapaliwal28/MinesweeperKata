bc9db015d75a8c6ca92c559693413f01e25bf836
David Whitlock <david.m.whitlock@gmail.com>
2017-07-26 19:31:55 -0700

    Worked with the class to write some unit test in order to build out the Minesweeper class.

---
+++ minesweeper/src/it/java/edu/pdx/cs410J/katas/RaggedBoardException.java
@@ -0,0 +1,4 @@
+package edu.pdx.cs410J.katas;
+
+public class RaggedBoardException extends Throwable {
+}
--- minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
+++ minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
@@ -2,4 +2,15 @@ package edu.pdx.cs410J.katas;
 
 public class Minesweeper  {
 
+  public Minesweeper(int row, int column) {
+
+  }
+
+  public void addRow(String row) {
+
+  }
+
+  public String solve() {
+    return null;
+  }
 }
